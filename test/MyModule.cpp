dae788c415353f3bc1258075fbb97ac04ee1208a
Kaoru <SHIINA_KAORU@outlook.com>
2023-03-20 14:15:00 +0800

    test: 添加相关模块测试

---
+++ test/MyModule.cpp
@@ -0,0 +1,30 @@
+#include "sese/plugin/Marco.h"
+
+class Printable : public sese::plugin::BaseClass {
+    virtual void run() = 0;
+};
+
+class Bye : public Printable {
+public:
+    void run() override {
+        puts("Bye");
+    }
+};
+
+class Hello : public Printable {
+public:
+    void run() override {
+        puts("Hello");
+    }
+};
+
+DEFINE_MODULE_INFO(
+                .moduleName = "MyModule",
+                .versionString = "0.1.0",
+                .description = "The module for test."
+)
+
+DEFINE_CLASS_FACTORY(
+        REGISTER_CLASS("com.kaoru.plugin.test.Bye", Bye),
+        REGISTER_CLASS("com.kaoru.plugin.test.Hello", Hello)
+)
